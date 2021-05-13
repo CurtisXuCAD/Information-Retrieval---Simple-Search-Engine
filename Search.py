f839cc3a741ddc8e9c983686827f5a79225bebeb
CurtisXuCAD <CurtisXuCAD@users.noreply.github.com>
2021-05-14 07:01:37 +0800

    Version 1.1
    Modify ALL the function
    Add get_top_5_answer function to get the report

--- Search.py
+++ Search.py
@@ -1,68 +1,100 @@
 import glob
 import json
 import time
-import os
+import krovetz
+from nltk.corpus import stopwords
+from collections import defaultdict
+
+ks = krovetz.PyKrovetzStemmer()
+stop_words = set(stopwords.words('english'))
 
 #   C:/Users/Owner/OneDrive/Desktop/CS121/Assignment3 M1/words_index\c
-def get_file_path(folderName):
+def get_file_path(query_word):
     """
-    get_file_path() take a path-like string (foldername) and return all files' paths in that folder and its subfolders
+    get_file_path() take a query word and its index file path
     """
-    file_paths = []  # list to store file paths
-    folderName = folderName + "/*"
-
-    for subfolder in glob.iglob(folderName):
-        subfolder = subfolder
-        #print(subfolder)
-        for single_file_path in glob.iglob(subfolder):
-            file_paths.append(single_file_path)
-    return file_paths
+    index_files_path = "D:\\OneDrive\\ICS\\CS 121\\CS121 Assignment\\Assignment3 M1\\words_index"
+    query_index_path = ""
+    if len(query_word) > 1:
+        query_index_path = index_files_path + "\\" + query_word[0] + "\\" + query_word[:2] + ".json"
+    else:
+        query_index_path = index_files_path + "\\" + query_word[0] + query_word[0] + ".json"
+    return query_index_path
 
 def extract_content(json_file, query_word):
-    query_list = []
-    with open(json_file) as file:        #  open json file   ex: cr file
-        json_data = json.load(file)
-        for data in json_data.keys():
-            if data == query_word:           #data word == query word
-                query_list.append(json_data[query_word])     #save into list
-    return set(query_list)                 # set of list urls for query_word
+    query_index_dict = dict()
+    with open(json_file,'r',encoding='utf-8') as file:        #  open json file   ex: cr file
+        data = json.load(file)
+        query_index_dict = data[query_word] #get the index matrix
+    return query_index_dict                # return the matrix
 
 #   1 cristina lopes, 2 machine learning, 3 ACM, 4 master of software engineering
 def search_query(query):
     #   words = ['cristina', 'lopes'] etc..
     words = []
-    words_urls = []
+    query_indexes= [] # list to store word's index dict
     query = query.split()
+    # tokenize the query
     for word in query:
-        words.append(word.lower())
-    #   list of file_paths
-    file_path = get_file_path("C:/Users/Owner/OneDrive/Desktop/CS121/Assignment3 M1/words_index")
-    words_count = 0
-    for paths in file_path:     #first path in list
-        for word in words:    #first query in list
-            if paths[-1] == word[0]:      #check last element path == first chart in query
-                words_urls.append(extract_content(paths, word))   #append query word list
-    return words_urls
+        # if word not in stop_words: # remove stop words
+            words.append(ks.stem(word))
+    
+    for w in words:
+        query_index_path = get_file_path(w)
+        
+        try:
+            #append query word's index dict
+            query_indexes.append(extract_content(query_index_path, w))
+                       
+        except:
+            #if file not found exception or can find word in matrix, it mean there is no indexed about this word
+            print(f"Can't find anything about \"{w}\" !")
+            query_indexes.append({})
+    
+    return query_indexes
 
-# AND , merge query
-def intersect(words_urls):
-    answer = []
-    if len(words_urls) <= 1:    # if one list in the url list return answer :  ex: acm
-        return answer
-        # url lists [{'cristina list'}, {'lopes list'}]
-        # One-Liner to intersect a list of sets
-    answer = words_urls[0].intersection(*words_urls)
+# merge query
+def intersect(query_indexes):
+    answer = defaultdict(int)
+    
+    #
+    intersections = query_indexes[0].keys()
+    for i in query_indexes:
+        intersections = intersections & i.keys()
+    
+    #calculate the total frequency
+    for i in intersections:
+        for qi in query_indexes:
+            answer[i] += qi[i]
+        
     return answer
 
 # what makes top 5 URLs?
+
+def get_top_5_answer(answer):
+    sorted_answer = sorted(answer.keys(), key=lambda x: answer[x], reverse=True)
+    i = 0
+    while True:
+        if i >= 5 or i >= len(sorted_answer):
+            break
+        print(sorted_answer[i])
+        i += 1
+
+
 # what does screenshot of your search interface in text?
 
 if __name__ == '__main__':
-   # file_paths = get_file_path("C:/Users/Owner/OneDrive/Desktop/CS121/Assignment3 M1/words_index")
-    query = input("Please type in the key word: ")
-    res = []
-    words_urls = search_query(query)
-    if len(words_urls) != 0:
-        answer = intersect(words_urls)
-    else:
-        print("can't find any thing, please search again! ")
+    # file_paths = get_file_path("C:/Users/Owner/OneDrive/Desktop/CS121/Assignment3 M1/words_index")
+    while True:
+        query = input("\nPlease type in the key word: (Press ENTER directly to EXIT)\n")
+        print()
+        if query == "":
+            print("END")
+            break
+        answer = intersect(search_query(query))
+        if len(answer) != 0:
+            get_top_5_answer(answer)
+        else:
+            print(f"There are no results about \"{query}\" !")
+    
+    
