0cc6288faca9add4cc7e97c2b1618e1a1c6c4a6f
CurtisXuCAD <CurtisXuCAD@users.noreply.github.com>
2021-05-29 12:45:04 +0800

    fix readme

--- test.py
+++ test.py
@@ -1,3 +1,12 @@
-a = 1
-b = 2
-print(format(a-b, '.3f'))
+# import Search as s
+# import time
+# start = time.clock()
+# a = 1
+# b = 2
+# print(format(a-b, '.3f'))
+# print(2.0**2)
+# print(s.query_prod(["acm","lopes"]))
+# end = time.clock()
+# print(f"Found results ({format(end-start,'.3f')})seconds")
+
+from flask import Flask
